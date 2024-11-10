76bb1d50437c31e1e3a5d0fef1dd19338281b581
ice breaker <1324318532@qq.com>
2024-11-11 01:01:50 +0800

    chore: init app

---
+++ apps/server/src/main.ts
@@ -0,0 +1,9 @@
+import process from 'node:process'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(process.env.PORT ?? 3000)
+}
+bootstrap()
