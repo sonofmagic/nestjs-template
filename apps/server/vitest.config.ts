98f2ec6c8da273e09bc23d5fd14ea51916739342
ice breaker <1324318532@qq.com>
2024-11-11 01:14:38 +0800

    chore: add vitest

--- apps/server/vitest.config.ts
+++ apps/server/vitest.config.ts
@@ -1,4 +1,5 @@
 import path from 'node:path'
+import swc from 'unplugin-swc'
 import { defineProject } from 'vitest/config'
 
 export default defineProject({
@@ -11,5 +12,13 @@ export default defineProject({
     ],
     globals: true,
     testTimeout: 60_000,
+    root: './',
   },
+  plugins: [
+    // This is required to build the test files with SWC
+    swc.vite({
+      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+      module: { type: 'es6' },
+    }),
+  ],
 })
