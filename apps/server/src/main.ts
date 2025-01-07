4d0fa720b43eb54b430f828f13ee8a9a20abf68b
ice breaker <1324318532@qq.com>
2025-01-07 15:56:39 +0800

    chore: switch to express

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,15 +1,10 @@
 import process from 'node:process'
 import { NestFactory } from '@nestjs/core'
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
+  const app = await NestFactory.create(
     AppModule,
-    new FastifyAdapter({ logger: true }),
   )
   await app.listen(process.env.PORT ?? 3000)
 }
