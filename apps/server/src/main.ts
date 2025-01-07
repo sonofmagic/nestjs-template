96b49567fc012c0ec7af81174c40035db263627b
ice breaker <1324318532@qq.com>
2025-01-07 16:36:44 +0800

    chore(deps): add cores

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(
     AppModule,
+    {
+      cors: true,
+    },
   )
   await app.listen(process.env.PORT ?? 3000)
 }
